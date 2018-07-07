7c021d415851b35d88f39a8d822a6490adf3ab3d
OTerrien <oterrien@neuf.fr>
2018-07-07 12:37:52 +0200

    update

--- src/main/java/com/ote/test/TestServiceRunner.java
+++ src/main/java/com/ote/test/TestServiceRunner.java
@@ -1,12 +1,24 @@
 package com.ote.test;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class TestServiceRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TestServiceRunner.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(@Value("${spring.application.name}") String applicationName){
+        return (args) -> {
+            System.out.println(applicationName);
+        };
     }
 }
